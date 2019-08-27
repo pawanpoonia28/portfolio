88e16c83a209613e126d36e8d96ef60e6cfbe55a
pawanpoonia28 <52160871+pawanpoonia28@users.noreply.github.com>
2019-08-27 12:01:59 +0530

    Add files via upload

---
+++ for.py
@@ -0,0 +1,132 @@
+# for i in range(5):
+# 	print("navgurukul")
+
+# for i in range(2,21,2):
+# 	print(i)
+
+# for i in range(100):
+# 	if i%7==0:
+# 		print(i)
+
+
+# for i in range(101):
+# 	print(i)
+
+
+# a=0
+# for i in range(100):
+# 	a+=i
+# 	print(a)
+
+
+# a=0
+# for i in range(10):
+# 	b=int(input("enter the input"))
+# 	a+=b
+# print(a)
+
+# for i in range(101):
+# 	if i%2==0:
+# 		print(-i)
+# 	else:
+# 		print(i)
+
+
+# for i in range(556,656):
+# 	a=i-555
+# 	if a%7==0:
+# 		print(a)
+
+
+# for i in range(101):
+# 	if i%3==0 and i%7==0:
+# 		print("pawan kumar")
+# 	elif i%7==0:
+# 		print("kumar")
+# 	elif i%3==0:
+# 		print("pawan")
+# 	else:
+# 		print(i)
+
+
+# p=10
+# for i in range(1,10,2):
+# 	print(" "*p,"#"*i)
+# 	p-=1
+# for i in range(11,0,-2):
+# 	print(" "*p,"#"*i)
+# 	p+=1
+
+# for i in range(156,166):
+# 	a=i-155
+# 	print(a)
+
+# #sum print karna
+# a=0
+# p=int(input("enter the input"))
+# for i in range(p):
+# 	b=int(input("enter the input"))
+# 	a+=b
+# print(a)
+
+
+
+
+# a=int(input("number dalo"))
+# for i in range(2,a):
+# 	if a%i==0:
+# 		print("this is not prime number ")
+# 		break
+# else:
+# 	print("this is prime number")
+
+
+
+# a=" "
+# for i in range(5,0,-1):
+# 	a=str(i)
+# 	print(a*5)
+
+# a=0
+# for i in range(30,420):
+# 	if i%8==0:
+# 		a+=i
+# 		print(a)
+
+
+
+# # averge print karna
+# a=0
+# p=int(input("enter the range"))
+# for i in range(p):
+# 	b=int(input("enter the input"))
+# 	a+=b
+# 	c=a//p
+# print(c)
+# if c%p==0:
+# 	print("hai")
+# else:
+# 	print("nhi hai")
+
+
+
+
+
+# import random
+# p=(random.randint(1,10))
+# name= input("inter the name\n ")
+# print(f"hello {name} welcome the game")
+# for i in range(5,0,-1):
+# 	b=int(input("guess the number 1 to 10  "))
+# 	print("")
+# 	if b==p:
+# 		print("your number is  correct")
+# 		break
+# 	elif b>p:
+# 		print("ap  10  se bada input diya ha")
+# 		print("you have only" ,i-1, "chansha\n")
+# 	else:
+# 		print("you have only" ,i-1, "chansha\n")
+
+
+
